8bce602392a075214f1924d04b6f4b603dfb5783
creeper2556 <102348050+creeper2556@users.noreply.github.com>
2022-03-27 13:10:41 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,62 @@
 function L () {
-	
+    if (!(gg.get(LedSpriteProperty.X) == 0)) {
+        gg.move(-1)
+    } else {
+        gg.move(4)
+    }
 }
 input.onButtonPressed(Button.A, function () {
-    yy.move(-1)
+    L2()
 })
+function R2 () {
+    if (!(yy.get(LedSpriteProperty.X) == 4)) {
+        yy.move(1)
+    } else {
+        yy.move(-4)
+    }
+}
+function L2 () {
+    if (!(yy.get(LedSpriteProperty.X) == 0)) {
+        yy.move(-1)
+    } else {
+        yy.move(4)
+    }
+}
 input.onButtonPressed(Button.B, function () {
-    yy.move(1)
+    R2()
 })
-let zd: game.LedSprite = null
-let p = 0
-let yy: game.LedSprite = null
-yy = game.createSprite(2, 4)
-let gg = game.createSprite(2, 0)
-basic.forever(function () {
-    p = randint(-1, 1)
-    if (p == 0) {
+function R () {
+    if (!(gg.get(LedSpriteProperty.X) == 4)) {
         gg.move(1)
     } else {
-        gg.move(p)
+        gg.move(-4)
     }
-    basic.pause(100)
+}
+let zd: game.LedSprite = null
+let cd = 0
+let p = 0
+let gg: game.LedSprite = null
+let yy: game.LedSprite = null
+yy = game.createSprite(2, 4)
+gg = game.createSprite(2, 0)
+basic.forever(function () {
+    p = randint(0, 3)
+    if (p == 0) {
+        L()
+    } else {
+        if (p == 1) {
+            R()
+        }
+    }
+    basic.pause(500)
 })
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.AB)) {
+    cd += -1
+    basic.pause(1000)
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.AB) && cd <= 0) {
+        cd = 2
         zd = game.createSprite(yy.get(LedSpriteProperty.X), 3)
         zd.set(LedSpriteProperty.Direction, 0)
         while (!(zd.get(LedSpriteProperty.Y) == 0)) {
